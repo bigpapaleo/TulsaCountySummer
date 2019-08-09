ff2a20eabe3c9d5d82ce10929cf27ec87f1c2b41
Daniel Jackson <Daniel@createsomethinginteresting.com>
2019-08-08 21:26:32 -0500

    UX updates

--- TCS/Intermediate/Build/Win64/TCS/Development/Core/SharedPCH.Core.cpp
+++ TCS/Intermediate/Build/Win64/TCS/Development/Core/SharedPCH.Core.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Admin/Documents/GitHub/TulsaCountySummer/TCS/Intermediate/Build/Win64/TCS/Development/Core/SharedPCH.Core.h"
+#include "C:/Users/danie/Documents/GitHub/TulsaCountySummer/TCS/Intermediate/Build/Win64/TCS/Development/Core/SharedPCH.Core.h"
